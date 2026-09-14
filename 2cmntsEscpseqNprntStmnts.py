0ea4ca3c02705ed165c9b0c7101556c1f74ecc8b
Moulali <shaikmoulali.0309@gmail.com>
2026-09-15 01:29:33 +0530

    updated name and it is a program of commments, escape sequence and print statements

---
+++ 2cmntsEscpseqNprntStmnts.py
@@ -0,0 +1,4 @@
+print("here i am gonna learn and write \nescape sequence and print statements in python")# by using \n we can print in new line
+print("here i learnt about comments by using ' # 'we can write comments in python")
+#this is a comment
+print("hey", 6, 7, sep='~', end='12\n')# by using sep we can separate the values in print statement
