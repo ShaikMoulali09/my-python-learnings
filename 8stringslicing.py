8fa5bad47afd7d624b36236187917589dec5b769
Moulali <shaikmoulali.0309@gmail.com>
2026-09-15 01:39:19 +0530

    program implementing String Slicing

--- 8stringslicing.py
+++ 8stringslicing.py
@@ -2,7 +2,7 @@ s="Shaik Moulali"
 print(len(s)) #prints the length of the string
 print(s[0:13]) #prints the string from index 0 to 12
 print(s[0:13:2]) #prints the string from index 0 to 12 with a step of 2
-print(s[:-4]) #prints the string from index 0 to 8, 13-4=9 prints upto index 8
+print(s[0:-4]) #prints the string from index 0 to 8, 13-4=9 prints upto index 8
 print(s[-4:-2]) #prints the string from index 9 to 10, 13-4=9 and 13-2=11 prints upto index 10
 print(s[::-1]) #prints the string in reverse order
 print(s[::2]) #prints the string with a step of 2
