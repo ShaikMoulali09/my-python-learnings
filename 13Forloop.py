7ab7e78a69597a8eb13ca4e8d398426528078b9c
Moulali <shaikmoulali.0309@gmail.com>
2026-09-15 08:35:16 +0530

    deleting old file

---
+++ 13Forloop.py
@@ -0,0 +1,12 @@
+# loops is used to print something repeatedly for certain number of times
+m = "Moulali Shaik"
+for i in m:
+    print(i)
+    if(i=="a"):
+       print("this is a powerful name")
+
+names=["janbi","Jakeera","Naaziya","Nazeer"]
+for name in names:
+    print(name)
+    for n in name:
+     print(n)
